bd5158507f6a187e05841087762cd01d77fd504e
Martin Czygan <martin.czygan@gmail.com>
2024-03-18 18:53:11 +0100

    fix example

--- examples/xmlstream/main.go
+++ examples/xmlstream/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"encoding/xml"
+	"io"
 	"log"
 	"os"
 
@@ -37,19 +38,22 @@ func (ts *TagSplitter) Split(data []byte, atEOF bool) (advance int, token []byte
 		ts.pos = 0
 	}()
 	if atEOF {
+		log.Printf("eof: %d, %d", ts.pos, ts.buf.Len())
 		// at the end, just return the rest
 		// TODO: make sure we have a proper end tag (data may be broken)
 		ts.buf.Write(data)
-		return len(data), ts.buf.Bytes(), nil
+		return len(data), ts.buf.Bytes(), io.EOF
 	}
 	for {
 		if ts.batchSize == ts.count {
 			ts.count = 0
 			b := ts.buf.Bytes()
 			ts.buf.Reset()
+			log.Printf("found batch: %d", len(b))
 			return ts.pos, b, nil
 		}
 		if ts.in {
+			log.Printf("inside tag: %d", ts.pos)
 			v := bytes.Index(data[ts.pos:], ts.closing)
 			if v == -1 {
 				// current tag exceeds data, so write all and exit Split
