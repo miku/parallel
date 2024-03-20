99e4114430850d9c7622cce9bf937e72e90f0480
Martin Czygan <martin.czygan@gmail.com>
2024-03-20 13:02:09 +0100

    rework split

--- record/split.go
+++ record/split.go
@@ -16,7 +16,8 @@ type TagSplitter struct {
 	closing   []byte       // the closing tag to look for
 	pos       int          // current read position within data
 	in        bool         // whether we are inside the tag or not
-	buf       bytes.Buffer // read buffer
+	buf       bytes.Buffer // accumulated batch data
+	scratch   bytes.Buffer // intermediate read buffer
 	count     int          // the number of elements in the buffer so far
 	done      bool         // signals that we are done processing
 }
@@ -31,7 +32,6 @@ func NewTagSplitter(tag string) *TagSplitter {
 	}
 }
 
-// Split finds elements in the stream and will accumulate them up to a given batch size.
 func (ts *TagSplitter) Split(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	defer func() {
 		ts.pos = 0
@@ -40,12 +40,12 @@ func (ts *TagSplitter) Split(data []byte, atEOF bool) (advance int, token []byte
 		return 0, nil, io.EOF
 	}
 	if atEOF {
-		// at the end, just return the rest; we do not care, if there is a
-		// proper end tag, that is the problem of the calling code
-		//
-		// if we return io.EOF, Scan() would stop immediately, hence we set
-		// done to true and return in the subsequent call, only
+		// we assume that we do not have to care about exact batch counts at
+		// this point, but we can just return whatever is left
 		ts.done = true
+		if ts.scratch.Len() > 0 {
+			_, _ = io.Copy(&ts.buf, &ts.scratch)
+		}
 		if len(data) == 0 && ts.buf.Len() == 0 {
 			return 0, nil, nil
 		} else {
@@ -55,49 +55,72 @@ func (ts *TagSplitter) Split(data []byte, atEOF bool) (advance int, token []byte
 			return len(data), ts.buf.Bytes(), nil
 		}
 	}
+	// Possible cases. There may be iterations of split that do not return a
+	// token at all.
+	//
+	// | start | end | action                             |
+	// |-------|-----|------------------------------------|
+	// | 0     | 0   | move on                            |
+	// | 1     | 0   | put data in scratch buffer         |
+	// | 0     | 1   | collect scratch and data (count++) |
+	// | 1     | 1   | collect data (count++)             |
+	// | N     | N   | ...                                |
 	for {
 		if ts.BatchSize == ts.count {
-			// TODO: check for nested tags with the same name
-			ts.count = 0
 			b := ts.buf.Bytes()
 			ts.buf.Reset()
+			ts.count = 0
 			return ts.pos, b, nil
 		}
 		if ts.in {
+			// find closing tag
 			v := bytes.Index(data[ts.pos:], ts.closing)
 			if v == -1 {
-				// current tag exceeds data, so write all and exit Split
-				if n, err := ts.buf.Write(data[ts.pos:]); err != nil {
-					return n, nil, err
-				}
+				// we need to keep the data from the previous iteration around
+				_, _ = ts.scratch.Write(data[ts.pos:])
 				return len(data), nil, nil
 			} else {
-				// end tag found, write and increase counter
-				if _, err = ts.buf.Write(data[ts.pos : ts.pos+v]); err != nil {
-					return 0, nil, err
+				// found a closing tag
+				end := ts.pos + v + len(ts.closing)
+				// we do not support nested tags of the same name just yet, so we fail
+				if ts.openingTagCount(append(ts.scratch.Bytes(), data[ts.pos:end]...)) > 1 {
+					return 0, nil, ErrNestedTagsWithSameNameNotImplemented
 				}
-				if _, err = ts.buf.Write(data[ts.pos+v : ts.pos+v+len(ts.closing)]); err != nil {
-					return 0, nil, err
+				// if we have anything in the scratch buffer, add that first
+				if ts.scratch.Len() > 0 {
+					_, _ = io.Copy(&ts.buf, &ts.scratch)
+					ts.scratch.Reset()
 				}
-				ts.in = false
+				_, _ = ts.buf.Write(data[ts.pos:end])
+				ts.pos = end
 				ts.count++
-				ts.pos = ts.pos + v + len(ts.closing)
+				ts.in = false
 			}
 		} else {
-			// search for the next opening tag
-			v := bytes.Index(data[ts.pos:], append(ts.opening, '>'))
+			// find opening tag
+			v := ts.openingTagIndex(data[ts.pos:])
 			if v == -1 {
-				// make sure, we do not have a prefix
-				v = bytes.Index(data[ts.pos:], append(ts.opening, ' '))
-			}
-			if v == -1 {
-				// nothing found in rest of data, move on
 				return len(data), nil, nil
 			} else {
-				// found start tag
-				ts.in = true
 				ts.pos = ts.pos + v
+				ts.in = true
 			}
 		}
+	}
+}
+
+// openingTagCount counts the number of opening tags found in data.
+func (ts *TagSplitter) openingTagCount(data []byte) int {
+	return bytes.Count(data, append(ts.opening, '>')) + bytes.Count(data, append(ts.opening, ' '))
+}
+
+// openingTagIndex returns the index of the opening tag in data, or -1.
+func (ts *TagSplitter) openingTagIndex(data []byte) (index int) {
+	index = bytes.Index(data, append(ts.opening, '>'))
+	if index == -1 {
+		// make sure, we do not have a prefix
+		return bytes.Index(data, append(ts.opening, ' '))
+	} else {
+		return index
 	}
 }
