554721e770157c6117ee4d6dc7eb91e20f512bc6
Martin Czygan <martin.czygan@gmail.com>
2024-03-22 11:38:23 +0100

    tweaks

--- record/split.go
+++ record/split.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"slices"
+	"sync"
 )
 
 const defaultMaxBytes = 16777216
@@ -20,13 +21,31 @@ var (
 // approximately MaxBytesApprox bytes. It is guaranteed that each batch
 // contains at least one complete element content.
 type TagSplitter struct {
-	Tag            string
-	MaxBytesApprox uint         // max bytes to read per batch, approximately, 16MB by default
-	buf            []byte       // temporary storage
-	batch          bytes.Buffer // collected content
-	done           bool         // are we done processing
+	// Tag to split on. Nested tags with the same name are not supperted
+	// currently (they will cause an error).
+	Tag string
+	// MaxBytesApprox is the approximate number of bytes in a batch. A batch
+	// will always contain at least one element, which may exceed this number
+	// significantly. By default, we use 16M per batch.
+	MaxBytesApprox uint
+	// buf is the internal scratch space that is used to find a complete
+	// element. This buffer will grow as large as required to accomodate a tag.
+	buf []byte
+	// batch is the staging space to write complete tags to and its size will
+	// be approximate limited by MaxBytesApprox.
+	batch bytes.Buffer
+	// done signals when there is nothing more to return.
+	done bool
+	// once for initializing the opening and closing tag byte slices
+	once sync.Once
+	// the closing tag to look for (this does not change)
+	closingTag []byte
+	// opening tags variants, e.g. '<a>', and '<a '
+	openingTag1 []byte
+	openingTag2 []byte
 }
 
+// maxBytes returns the maximum byte size per batch.
 func (s *TagSplitter) maxBytes() int {
 	if s.MaxBytesApprox == 0 {
 		return defaultMaxBytes
@@ -35,12 +54,28 @@ func (s *TagSplitter) maxBytes() int {
 	}
 }
 
-// pruneBuf shrinks the internal buffer, if possible.
+// pruneBuf shrinks the internal buffer, if possible. The internal buffer shall
+// never be larger than 16K or twice the size of the byte slice passed to Split
+// (whichever is larger). The byte slice passed to Split is typically "getconf
+// PAGE_SIZE" on Linux.
+//
+// Currently, the median buffer size is about 3K.
+//
+//	In [6]: df = pd.read_csv("buffersize.tsv")
+//	In [7]: df.describe()
+//	Out[7]:
+//
+//	count 3701472.000
+//	mean     3770.982
+//	std      3641.797
+//	min         0.000
+//	25%      1561.000
+//	50%      3126.000
+//	75%      5048.000
+//	max    289179.000
 func (s *TagSplitter) pruneBuf(data []byte) {
-	// We want the internal buffer to be at least twice the size of the data
-	// buffer, as a tag may just be on a boundary, so 2 * len(data). If the
-	// data passed is too small, we want to accumulate at least a certain
-	// number of bytes, they could accomodate an XML tag (e.g. 16K).
+	// If the data passed is too small, we want to accumulate at least a
+	// certain number of bytes, they could accomodate an XML tag (e.g. 16K).
 	if L := slices.Max([]int{2 * len(data), 16384}); len(s.buf) < L {
 		return
 	}
@@ -48,6 +83,21 @@ func (s *TagSplitter) pruneBuf(data []byte) {
 	s.buf = s.buf[k:]
 }
 
+func (s *TagSplitter) ensureTags() {
+	if len(s.closingTag) == 0 {
+		s.closingTag = []byte("</" + s.Tag + ">")
+	}
+	if len(s.openingTag1) == 0 {
+		s.openingTag1 = []byte("<" + s.Tag + ">")
+	}
+	if len(s.openingTag2) == 0 {
+		s.openingTag2 = []byte("<" + s.Tag + " ")
+	}
+}
+
+// Split accumulates one or more XML element contents and returns a batch of
+// them as a token. This can be used for downstream XML parsing, where the
+// consumer expects a valid tag.
 func (s *TagSplitter) Split(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if s.Tag == "" {
 		return 0, nil, ErrTagRequired
@@ -55,6 +105,9 @@ func (s *TagSplitter) Split(data []byte, atEOF bool) (advance int, token []byte,
 	if s.done {
 		return 0, nil, io.EOF
 	}
+	s.once.Do(func() {
+		s.ensureTags()
+	})
 	s.buf = append(s.buf, data...)
 	for {
 		if s.batch.Len() >= s.maxBytes() {
@@ -74,6 +127,7 @@ func (s *TagSplitter) Split(data []byte, atEOF bool) (advance int, token []byte,
 				if s.batch.Len() == 0 {
 					return len(data), nil, nil
 				}
+				// return the rest of the batch, completely
 				return len(data), s.batch.Bytes(), nil
 			} else {
 				return len(data), nil, nil
@@ -101,19 +155,20 @@ func (s *TagSplitter) copyContent(w io.Writer) (n int, err error) {
 		return 0, ErrGarbledInput
 	}
 	last = end + len(s.Tag) + 3
-	// sanity check, TODO: fix this
+	// sanity check, TODO: fix this w/ a stack
 	if s.indexOpeningTag(s.buf[start+1:end]) != -1 {
 		return 0, ErrNestedTagsNotImplemented
 	}
 	n, err = w.Write(s.buf[start:last])
-	s.buf = s.buf[last:] // TODO: optimize this
+	s.buf = s.buf[last:] // TODO: optimize this, ringbuffer?
 	return
 }
 
 // https://www.w3.org/TR/REC-xml/#sec-starttags
 func (s *TagSplitter) indexOpeningTag(data []byte) int {
-	u := bytes.Index(data, []byte("<"+s.Tag+">"))
-	v := bytes.Index(data, []byte("<"+s.Tag+" "))
+	// Do not care too much about repetition, the data may well be cached..
+	u := bytes.Index(data, s.openingTag1)
+	v := bytes.Index(data, s.openingTag2)
 	if u == -1 && v == -1 {
 		return -1
 	}
@@ -130,5 +185,5 @@ func (s *TagSplitter) indexOpeningTag(data []byte) int {
 // indexClosingTag returns the index of the next closing tag in a given byte
 // slice.
 func (s *TagSplitter) indexClosingTag(data []byte) int {
-	return bytes.Index(data, []byte("</"+s.Tag+">"))
+	return bytes.Index(data, s.closingTagl)
 }
